6006b18c91f186ad475c24a05f28edeb011ccf16
jaezeu <jazeel.meerasah@gmail.com>
2026-09-23 19:53:29 +0800

    fmt

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket       = "sctp-tfstate-ce13"
-    key          = "jaz-gha-s3.tfstate"
-    region       = "us-east-1"
+    bucket = "sctp-tfstate-ce13"
+    key    = "jaz-gha-s3.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-   region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "workshop" {
