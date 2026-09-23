7d607152a99741988e5f0f192d71aed3d78c8d30
jaezeu <jazeel.meerasah@gmail.com>
2026-09-23 20:00:21 +0800

    tflint

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
   backend "s3" {
     bucket = "sctp-tfstate-ce13"
     key    = "jaz-gha-s3.tfstate"
